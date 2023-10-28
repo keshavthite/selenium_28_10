9143ed6042a9de12152b3bd064e8a7c9b3009485
keshav <keshavthite9@gmail.com>
2023-10-28 14:22:57 +0530

    first commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass {
+    @Test
+    void test1()
+    {
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+        String title=driver.getTitle();
+        Assert.assertEquals(title,"Google");
+    }
+}
